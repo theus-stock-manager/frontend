1369af115f2c74bc394f2efee13b571803853fe9
Theus <matheusth.dev@gmail.com>
2023-06-09 12:50:33 -0300

    feat: função para ler todos os usuários

--- src/app/staff-panel/users/page.tsx
+++ src/app/staff-panel/users/page.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import * as B from "@/blocks";
 import * as C from "@/components";
 import { IUser } from "@/globalTypes/user";
+import api from "@/services/api";
 import { useState } from "react";
-("use client");
+import { toast } from "react-toast";
 
 export default function StaffUsersPage() {
   const [users, setUsers] = useState<IUser[]>([]);
+  const [nextUrl, setNextUrl] = useState<string | null>(null);
+  const [prevUrl, setPrevUrl] = useState<string | null>(null);
+  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(1);
   const [filterValue, setFilterValue] = useState("");
   const [isOpenCreate, setIsOpenCreate] = useState(false);
+
+  const token = localStorage.getItem("@SM-TOKEN");
+
+  const getUsers = async () => {
+    try {
+      const response = await api.get("/users", {
+        headers: { Authorization: `Bearer ${token}` },
+        params: { page: 1, limit: 5 },
+      });
+
+      setUsers(response.data.results);
+      setNextUrl(response.data.next);
+      setPrevUrl(response.data.previous);
+      setCount(response.data.count);
+      setPage(response.data.page);
+    } catch (error) {
+      toast.error("problemas ao carregar os usuários");
+    }
+  };
 
   const filterUsers = async () => {};
 
@@ -24,7 +50,7 @@ export default function StaffUsersPage() {
       <h1 className="mb-4 text-4xl font-bold">Empresas</h1>
 
       <B.Filter
-        placeholder="Procurar empresas"
+        placeholder="Procurar usuários"
         filterValue={filterValue}
         onFilterChange={filterChange}
         onFilterAction={filterUsers}
