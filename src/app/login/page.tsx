1a2a0701bdc4821672164c06689b459bf5d8420f
Theus <matheusth.dev@gmail.com>
2023-06-16 20:40:23 -0300

    n

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 import Link from "next/link";
 import * as B from "@/blocks";
+import { FaHome } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
+
   return (
     <section className="flex items-center justify-center w-full h-full">
+      <FaHome
+        onClick={() => router.push("/")}
+        className="absolute text-4xl cursor-pointer top-4 left-4"
+      />
+
       <div className="w-[17.5rem] flex flex-col justify-center items-center gap-4 py-8 px-4 rounded-lg bg-[#636e72] custom-box-shadow">
         <h1 className="text-[16pt] text-white font-bold mb-2">
           Faça o seu login
